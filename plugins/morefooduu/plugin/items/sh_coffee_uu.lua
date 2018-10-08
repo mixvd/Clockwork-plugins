a8c639853826f5260b0ae5803cfa38b75a163d45
Quentin <thequentin47@gmail.com>
2018-10-08 11:14:22 +0200

    Update sh_coffee_uu.lua

--- plugins/morefooduu/plugin/items/sh_coffee_uu.lua
+++ plugins/morefooduu/plugin/items/sh_coffee_uu.lua
@@ -1,4 +1,4 @@
-local ITEM = Clockwork.item:New("consumable_base");
+local ITEM = Clockwork.item:New();
 ITEM.name = "UU Coffee";
 ITEM.cost = 0;
 ITEM.model = "models/bioshockinfinite/xoffee_mug_closed.mdl";
