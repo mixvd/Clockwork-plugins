134a728b70f56a309f010f8564556b38f6ad3ddb
Quentin <thequentin47@gmail.com>
2018-10-08 11:20:43 +0200

    Update sh_smooth_breens_water.lua

--- plugins/morefooduu/plugin/items/sh_smooth_breens_water.lua
+++ plugins/morefooduu/plugin/items/sh_smooth_breens_water.lua
@@ -1,11 +1,11 @@
 --[[
-	© 2013 CloudSixteen.com do not share, re-distribute or modify
+	Â© 2013 CloudSixteen.com do not share, re-distribute or modify
 	without permission of its author (kurozael@gmail.com).
 --]]
 
 local ITEM = Clockwork.item:New();
 ITEM.name = "Smooth Water Can";
-ITEM.cost = 12;
+ITEM.cost = 0;
 ITEM.skin = 1;
 ITEM.model = "models/props_lunk/popcan01a.mdl";
 ITEM.skin = 1;
