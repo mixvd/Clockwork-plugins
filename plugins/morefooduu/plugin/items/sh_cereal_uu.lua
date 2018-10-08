2431f32fd63d11bf768f7d80abdb7aa62cb73b01
Quentin <thequentin47@gmail.com>
2018-10-08 11:13:43 +0200

    Update sh_cereal_uu.lua

--- plugins/morefooduu/plugin/items/sh_cereal_uu.lua
+++ plugins/morefooduu/plugin/items/sh_cereal_uu.lua
@@ -1,4 +1,4 @@
-local ITEM = Clockwork.item:New("consumable_base");
+local ITEM = Clockwork.item:New();
 ITEM.name = "UU Cereal";
 ITEM.cost = 0;
 ITEM.model = "models/bioshockinfinite/hext_cereal_box_cornflakes.mdl";
