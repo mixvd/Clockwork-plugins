72a75d787034371db19715a5db3fce92dbff7463
Quentin <thequentin47@gmail.com>
2018-10-08 11:14:56 +0200

    Update sh_compl_cp.lua

--- plugins/morefooduu/plugin/items/sh_compl_cp.lua
+++ plugins/morefooduu/plugin/items/sh_compl_cp.lua
@@ -1,4 +1,4 @@
-local ITEM = Clockwork.item:New("consumable_base");
+local ITEM = Clockwork.item:New();
 ITEM.name = "Civil Protection Complement";
 ITEM.cost = 0;
 ITEM.model = "models/probs_misc/tobccco_box-1.mdl";
@@ -9,7 +9,7 @@ ITEM.category = "Consumables";
 ITEM.business = true;
 ITEM.description = "A transparent red pouch with strange pieces of meat.";
 
-function ITEM:OnUsedConsumable(player, itemEntity)
+function ITEM:OnUse(player, itemEntity)
 	player:BoostAttribute(self.name, ATB_STRENGTH, 3, 7200);
 	player:BoostAttribute(self.name, ATB_AGILITY, 3, 7200);
 	player:BoostAttribute(self.name, ATB_STAMINA, 3, 7200);
