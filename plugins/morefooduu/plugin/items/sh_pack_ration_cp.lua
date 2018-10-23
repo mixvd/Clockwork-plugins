8dfcd83000e9ae2bd0864cf1af09f7c4979273e1
Quentin <thequentin47@gmail.com>
2018-10-23 15:08:17 +0200

    Update sh_pack_ration_cp.lua

--- plugins/morefooduu/plugin/items/sh_pack_ration_cp.lua
+++ plugins/morefooduu/plugin/items/sh_pack_ration_cp.lua
@@ -7,7 +7,6 @@ ITEM.category = "Ration";
 ITEM.description = "A black plastic pouch containing the rationing of Civil Protection.\nIt contains:\n-1 Ration of Civil Protection.\n-1 Civil Protection Supplement.\n-1 Civil Protection Complement.\n-1 UU Coffee.";
 
 function ITEM:OnUse(player, itemEntity)
-
 	Clockwork.player:Notify(player, "You opened a rationing pack.");
 	player:GiveItem(Clockwork.item:CreateInstance("Ration of Civil Protection"));
 	player:GiveItem(Clockwork.item:CreateInstance("Civil Protection Supplement"));
