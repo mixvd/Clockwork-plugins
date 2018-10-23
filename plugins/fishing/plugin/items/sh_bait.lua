d31d2ea72336d017a162917693f9d8eaec119a46
Quentin <thequentin47@gmail.com>
2018-10-23 15:14:22 +0200

    Update sh_bait.lua

--- plugins/fishing/plugin/items/sh_bait.lua
+++ plugins/fishing/plugin/items/sh_bait.lua
@@ -2,7 +2,7 @@
 
 local ITEM = Clockwork.item:New();
 ITEM.name = "Bait";
-ITEM.model = "models/Items/item_item_crate.mdl";
+ITEM.model = "models/Items/item_item_crate.mdl"; -- change the model
 ITEM.weight = 0.3;
 ITEM.uniqueID = "bait";
 ITEM.category = "Fishing";
