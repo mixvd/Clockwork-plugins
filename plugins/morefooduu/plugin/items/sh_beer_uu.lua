59a4d65e444b11f18f10da39c6da6c305480aae0
Quentin <thequentin47@gmail.com>
2018-10-23 15:04:38 +0200

    Update sh_beer_uu.lua

--- plugins/morefooduu/plugin/items/sh_beer_uu.lua
+++ plugins/morefooduu/plugin/items/sh_beer_uu.lua
@@ -10,6 +10,9 @@ ITEM.useSound = {"npc/barnacle/barnacle_gulp1.wav", "npc/barnacle/barnacle_gulp2
 ITEM.business = true;
 ITEM.description = "A beer from the Union Universel, is fresh.";
 
+-- Called when a player uses the item.
+function ITEM:OnUse(player, itemEntity) end;
+
 -- Called when a player drops the item.
 function ITEM:OnDrop(player, position) end;
 
