b3759cb9b9b8ad8c6dc98e930bc283bcbd98af97
Quentin <thequentin47@gmail.com>
2018-10-23 15:08:33 +0200

    Update sh_pack_ration_supervisor.lua

--- plugins/morefooduu/plugin/items/sh_pack_ration_supervisor.lua
+++ plugins/morefooduu/plugin/items/sh_pack_ration_supervisor.lua
@@ -7,7 +7,6 @@ ITEM.category = "Ration";
 ITEM.description = "A red plastic pouch containing rationing from a supervisor.\nIt contains:\n-1 UU Coffee.\n-1 UU Chocolate.\n-1 UU Banana.\n-1 UU Peanut.\n-1 UU Cheese.\n-1 UU Corn.\n-1 UU Orange.\n-1 UU Bread.\n-1 UU Apple.\n-1 UU Pear.\n-1 UU Sardine.\n-1 UU Potato.\n-1 UU Pickles.\n-1 UU Cereal.\n-1 UU Pineapple.";
 
 function ITEM:OnUse(player, itemEntity)
-
 	Clockwork.player:Notify(player, "You opened a rationing pack.");
 	player:GiveItem(Clockwork.item:CreateInstance("UU Coffee"));
 	player:GiveItem(Clockwork.item:CreateInstance("UU Chocolate"));
