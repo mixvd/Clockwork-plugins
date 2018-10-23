05d08e7c741bc50d4ef3bbe1b3c6f54f42c245ed
Quentin <thequentin47@gmail.com>
2018-10-23 15:11:59 +0200

    Update sh_suppl_citizen_loyal.lua

--- plugins/morefooduu/plugin/items/sh_suppl_citizen_loyal.lua
+++ plugins/morefooduu/plugin/items/sh_suppl_citizen_loyal.lua
@@ -9,6 +9,9 @@ ITEM.category = "Consumables";
 ITEM.business = true;
 ITEM.description = "A transparent red pouch with a soft whitish paste.";
 
+-- Called when a player uses the item.
+function ITEM:OnUse(player, itemEntity) end;
+
 -- Called when a player drops the item.
 function ITEM:OnDrop(player, position) end;
 
